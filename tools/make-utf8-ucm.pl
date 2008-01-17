e762acdc45f2192245beea06a07fac93e8316f36
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2008-01-17 19:24:37 +0000

    Import of MIYAGAWA/Encode-JP-Mobile-0.20 from CPAN.
    gitpan-cpan-distribution: Encode-JP-Mobile
    gitpan-cpan-version:      0.20
    gitpan-cpan-path:         MIYAGAWA/Encode-JP-Mobile-0.20.tar.gz
    gitpan-cpan-author:       MIYAGAWA
    gitpan-cpan-maturity:     released

--- tools/make-utf8-ucm.pl
+++ tools/make-utf8-ucm.pl
@@ -3,14 +3,15 @@ use warnings;
 use YAML;
 use Encode;
 use Encode::JP::Mobile;
+use autobox;
+use autobox::Core;
+use autobox::Encode;
+use FindBin;
+use File::Spec;
+use Path::Class;
 
-my $map = YAML::LoadFile 'dat/convert-map-utf8.yaml';
-
-my %alias = qw(
-    docomo imode
-    kddi ezweb
-    softbank vodafone
-);
+my $map = YAML::LoadFile file($FindBin::Bin, '..', 'dat', 'convert-map-utf8.yaml');
+my $cp932_ucm = file($FindBin::Bin, '..', 'ucm', 'cp932.ucm');
 
 my $uni_range_for = {
     docomo   => Encode::JP::Mobile::InDoCoMoPictograms(),
@@ -18,53 +19,51 @@ my $uni_range_for = {
     softbank => Encode::JP::Mobile::InSoftBankPictograms(),
 };
 
-my $cp932_ucm = shift @ARGV or die "Usage: $0 cp932.ucm\n";
+sub SCALAR::to_hex($) { sprintf '%X', $_[0] }
+sub SCALAR::omote2ura($) { $_[0]->encode('x-sjis-kddi')->decode('x-sjis-kddi-auto') }
+sub SCALAR::uni2int($) { unpack 'U*', $_[0] }
 
-for my $to (qw( docomo kddi softbank )) {
-    open my $fh, '>', "ucm/x-utf8-$to.ucm" or die $!;
-    print {$fh} <<HEAD
-<code_set_name> "x-utf8-$to"
-<code_set_alias> "x-utf8-$alias{$to}"
-<mb_cur_min> 1
-<mb_cur_max> 2
-<subchar> \\x3F
-CHARMAP
-HEAD
-    ;
+&main;exit;
 
-    # basic map
-    print {$fh} unicode_ucm($cp932_ucm);
+sub main {
+    for my $to (qw( docomo kddi softbank )) {
+        my $fh = file('ucm', "x-utf8-$to.ucm")->openw or die $!;
 
-    # convert map
-    for my $from (qw( docomo kddi softbank )) {
-        next if $from eq $to;
+        print {$fh} header($to);
 
-        print {$fh} "\n\n# pictogram convert map ($from => $to)\n";
+        # basic map
+        print {$fh} unicode_ucm($cp932_ucm);
 
-        for my $srcuni (sort keys %{$map->{$from}}) {
-            my $dstuni = $map->{$from}{$srcuni}{$to} or next;
-            printf {$fh} "<U%s> %s |1 # %s\n", $srcuni, unihex2utf8hex($dstuni), comment_for($from);
+        # convert map
+        for my $from (qw( docomo kddi softbank )) {
+            next if $from eq $to;
+
+            print {$fh} "\n\n# pictogram convert map ($from => $to)\n";
+
+            for my $srcuni (sort keys %{$map->{$from}}) {
+                my $dstuni = $map->{$from}{$srcuni}{$to} or next;
+                printf {$fh} "<U%s> %s |1 # %s\n", $srcuni, unihex2utf8hex($dstuni), comment_for($from);
+            }
         }
-    }
 
-    # original
-    if ($to eq 'kddi') {
-        # ura-kddi
-        range_each($to, sub {
-            my $unicode = shift;
-            my $sjiscode = hex unpack "H*", encode('x-sjis-kddi', chr $unicode);
-            my $unihex = sprintf '%X', $sjiscode - 0x0700;
-            print {$fh} sprintf "<U%s> %s |0 # %s\n", $unihex, unihex2utf8hex($unihex), "UraKDDI pictogram";
-        });
-    } else {
-        range_each($to, sub {
-            my $unicode = shift;
-            my $unihex = sprintf '%X', $unicode;
-            print {$fh} sprintf "<U%s> %s |0 # %s\n", $unihex, unihex2utf8hex($unihex), "$to pictogram";
-        });
-    }
+        # original
+        if ($to eq 'kddi') {
+            # ura-kddi
+            range_each($to, sub {
+                my $unicode = shift;
+                my $unihex = $unicode->chr->omote2ura->uni2int->to_hex;
+                print {$fh} sprintf "<U%s> %s |0 # %s\n", $unihex, unihex2utf8hex($unihex), "UraKDDI pictogram";
+            });
+        } else {
+            range_each($to, sub {
+                my $unicode = shift;
+                my $unihex = $unicode->to_hex;
+                print {$fh} sprintf "<U%s> %s |0 # %s\n", $unihex, unihex2utf8hex($unihex), "$to pictogram";
+            });
+        }
 
-    print {$fh} "END CHARMAP\n";
+        print {$fh} "END CHARMAP\n";
+    }
 }
 
 sub comment_for {
@@ -75,11 +74,28 @@ sub comment_for {
       :                          "";
 }
 
+sub header {
+    my $to = shift;
+
+    my %alias = qw(
+        docomo imode
+        kddi ezweb
+        softbank vodafone
+    );
+
+    <<"HEAD";
+<code_set_name> "x-utf8-$to"
+<code_set_alias> "x-utf8-$alias{$to}"
+<mb_cur_min> 1
+<mb_cur_max> 2
+<subchar> \\x3F
+CHARMAP
+HEAD
+}
+
 sub unihex2utf8hex {
     my $uni = shift;
-    $uni = chr hex $uni;
-    $uni = encode 'utf-8', $uni;
-    $uni = unpack "H*", $uni;
+    $uni = 'H*'->unpack($uni->hex->chr->encode('utf-8'));
     $uni =~ s/(..)/\\x$1/g;
     $uni;
 }
@@ -87,7 +103,7 @@ sub unihex2utf8hex {
 sub unicode_ucm {
     my $cp932_ucm = shift;
     my $res = '';
-    open my $fh, '<', $cp932_ucm or die "$!: $cp932_ucm";
+    my $fh = $cp932_ucm->openr or die $!;
     while (my $line = <$fh>) {
         if ($line =~ /^<U(.{4})> \S+ \|0 # (.+)$/) {
             my ($unihex, $comment) = ($1, $2);
@@ -98,6 +114,7 @@ sub unicode_ucm {
             $res .= sprintf "<U%s> %s |0 # %s\n", $unihex, unihex2utf8hex($unihex), $comment;
         }
     }
+    $fh->close;
     $res;
 }
 
